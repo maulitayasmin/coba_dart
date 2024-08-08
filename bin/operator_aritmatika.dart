40e05aa3fb7d64290cde4e61c067d2658f36d787
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-08-09 01:53:50 +0700

    operator

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,11 @@
+void main(){
+  var h = 10;
+  var i = 3;
+
+  print(h + i);
+  print(h - i);
+  print(h * i);
+  print(h / i);
+  print(h ~/ i);
+  print(h % i);
+}
---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,14 @@
+void main(){
+
+  //menggunakan number
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  //menggunakan string
+  print('lita' == 'lita');
+  print('lita' != 'lita');
+}
