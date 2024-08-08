4bea5740c87ac72621ccbbcac08eb12d6fd025a5
Maulita Yasmin Nadila <maulitayasmin2@gmail.com>
2024-08-09 00:18:15 +0700

    number

---
+++ bin/num.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+
+  //jika ingin mengubah nilai dari variabel dengan tipe data int atau double maka menggunakan num + namaVariabel
+  //tidak berlaku jika menggunakan int/double + namaVariabel
+}
